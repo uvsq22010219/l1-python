62b7fbbba83c7d7f750683eac7c336717f532094
Djenna SEDDOUGUI <djenna.seddougui@ens.uvsq.fr>
2021-03-15 21:03:13 +0100

    ok

--- exercises/Balle/balle.py
+++ exercises/Balle/balle.py
@@ -13,6 +13,7 @@ nb_rebonds = 0
 def creer_balle():
     """Dessine un rond bleu et retourne son identifiant
      et les valeurs de déplacements dans une liste"""
+    global rayon
     x, y = LARGEUR // 2, HAUTEUR // 2
     dx, dy = 3, 5
     rayon = 20
@@ -24,8 +25,9 @@ def creer_balle():
 
 def mouvement():
     """Déplace la balle et ré-appelle la fonction avec un compte-à-rebours"""
-    if nb_rebonds < 30 :
+    if nb_rebonds < 30:
         rebond()
+        changercouleur()
         canvas.move(balle[0], balle[1], balle[2])
         canvas.after(20, mouvement)
 
@@ -34,21 +36,28 @@ def rebond():
     """Fait rebondir la balle sur les bords du canevas"""
     global balle, nb_rebonds
     x0, y0, x1, y1 = canvas.coords(balle[0])
-    if x1 <= 0:
+    if x1 <= 0 or x1 >= 600:
         nb_rebonds +=1
         balle[1] = -balle[1]
     
-    if x1 >= 600:
+    if y0 <= 0 or y0 >= 400:
         nb_rebonds +=1
-        balle[1] = -balle[1]
-    
-    if y0 <= 0 :
-        nb_rebonds +=1
-        balle[2] = -balle[2]
-    
-    if y0 >= 400:
-        nb_rebonds +=1
-        balle[2] = -balle[2]
+        balle[2] = -balle[2]    
+
+def changercouleur():
+    global balle, rayon
+    x0, y0, x1, y1 = canvas.coords(balle[0])
+    if y1 <= 50:
+        if 0 <= x0 < (150 - rayon):
+            canvas.itemconfig(balle[0], fill="red" )
+        elif 150 <= x0 < (300 - rayon):
+            canvas.itemconfig(balle[0], fill="green" )
+        elif 300 <= x0 < (450 - rayon):
+            canvas.itemconfig(balle[0], fill="blue" )
+        elif 450 <= x0 <= (600 - rayon):
+            canvas.itemconfig(balle[0], fill="yellow" )
+
+        
 
 ######################
 # programme principal
